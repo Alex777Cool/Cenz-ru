351298e3008957028d94265e5bf8e13f827e9034
Alex777Cool <106296907+Alex777Cool@users.noreply.github.com>
2022-10-24 21:22:42 +0700

    Update cenz-ru.py

--- cenz-ru.py
+++ cenz-ru.py
@@ -6,5 +6,5 @@ bad_words = json.load(open("cenz-ru.json"))
 
 for i in msg.text.split(" "):
     for b in bad_words:
-        if  fuzz.ratio(i.lower(), b) >= 40:
+        if  fuzz.ratio(i.lower(), b) >= 60:
             print("Найден мат в строке")
